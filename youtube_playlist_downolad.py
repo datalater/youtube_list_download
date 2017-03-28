3184b6ad91ddfa05a2e96ac484888b06441900e0
datalater <the7mincheol@gmail.com>
2017-03-28 18:42:52 +0900

    재생목록 or 개별영상 옵션 추가 (javascript 아직 해결 안됨)

--- youtube_playlist_downolad.py
+++ youtube_playlist_downolad.py
@@ -4,12 +4,20 @@ from bs4 import BeautifulSoup
 import youtube_dl # pip install --upgrade youtube-dl
 
 
-def download_mp4(mp4_list):
+def list_download(mp4_list):
     for mp4 in mp4_list:
         ydl_opts = {}
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([mp4])
-            
+
+
+def single_download(single_url):
+    try:
+        list_download([single_url])
+    except requests.exceptions.MissingSchema:
+        print("올바른 url이 아닙니다. ex) https://www.youtube.com/watch?v=hy0rMH2yeg8")
+    except KeyboardInterrupt:
+        print("Keybaord에 의해 중단되었습니다.")
 
 
 def parsing_list(playlist_url):
@@ -29,7 +37,7 @@ def parsing_list(playlist_url):
         print(user_question)
         if user_question == "y" or user_question == "Y":
             print("중간에 멈추려면 ctrl+c를 입력하세요.")
-            download_mp4(mp4_list)
+            list_download(mp4_list)
         else:
             print("프로그램을 멈추겠습니다.")
     except requests.exceptions.MissingSchema:
@@ -39,6 +47,12 @@ def parsing_list(playlist_url):
 
 if __name__ == "__main__":
     # playlist_url = "https://www.youtube.com/user/goodboy1384/videos"
+    # single_url = "https://www.youtube.com/watch?v=hy0rMH2yeg8"
     mp4_list = []
-    playlist_url = str(input("="*40+"\n"+"youtube 재생목록 url을 입력하세요."+"\n"+"ex) https://www.youtube.com/user/goodboy1384/videos"+"\n"+"="*40+"\n"+":"))
-    parsing_list(playlist_url)
+    user_option = input("재생목록으로 다운로드 하려면 'Y', 개별 영상 URL로 다운로드 하려면 'N'을 입력해주세요. \n: ")
+    if user_option == ("y" or "Y"):
+        playlist_url = str(input("="*40+"\n"+"youtube 재생목록 url을 입력하세요."+"\n"+"ex) https://www.youtube.com/user/goodboy1384/videos"+"\n"+"="*40+"\n"+":"))
+        parsing_list(playlist_url)
+    else:
+        single_url = str(input("="*40+"\n"+"youtube 개별영상 url을 입력하세요."+"\n"+"ex) https://www.youtube.com/watch?v=hy0rMH2yeg8"+"\n"+"="*40+"\n"+":"))
+        single_download(single_url)
